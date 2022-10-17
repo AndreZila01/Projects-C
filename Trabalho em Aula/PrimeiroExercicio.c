3dbeca5b06108bc5e17fecdb3ae2e1bac4432a94
AndreZila01 <andrecustodiotablet@gmail.com>
2022-10-17 22:25:02 +0100

    Try to solve problem of teacher
    Resolvido alguns exercicios, falta a parte 5...

--- Trabalho em Aula/PrimeiroExercicio.c
+++ Trabalho em Aula/PrimeiroExercicio.c
@@ -2,34 +2,50 @@
 #include <math.h>
 
 int main() {
-    printf("olá mundo");
-    printf("\"olá mundo\"");
-    int mult = 4*4;
-    double div = 20/3;
-    double result = pow(7, 2);
+    //Parte 1
+    //1.
+    printf("olá mundo");//a
+    printf("\"olá mundo\"");//b
+    int valuec = 4*4;//c
+    int valued = 3*1000;//d
+    long v1 = 3000000;
+	long v2 = 9999999;
+	long dsss = v1*v2;//e
+    double raizsete = sqrt(177);//f
+    double div = 20/3;//g
+    double result = pow(7, 2);//h
     int sum = 0;
     for (int i = 0; i <= 5; ++i) {
         sum += i;
-    }
-    double raiz = sqrt(50);
+    }//i
+
+
+    printf("\n");
+    //Parte 2
+    double rectangulo = 5 * 10;//a - retangulo
+    double Trapezoide = ((10+10)/2)*20;//b - trapezio
+    double triangulo = sqrt(10((10-7)(10-2)(10-5)));//c - triangulo
+    
 
     printf("\n");
 
-    int count =0;
-    do{
-        count++;
-    }while (count==count++);
-    
+    //Parte 3 Numeros Primos
+    printf("%d", primo(10));
+    //Parte 4
     int valuep=0, values=1;
     printf("%d\n", valuep);
-    while (valuep!=-1)
+    while (valuep < 143)
     {
-        valuep++;
-        printf("%d\n", valuep);
-        printf("%d\n", values);
-        valuep = values+valuep;
+       values = values + valuep;
+	   printf("%d\n", values);
+	   valuep = values + valuep;
+	   printf("%d\n", valuep);
     }
     
     
     return 0;
+}
+
+int primos(int numero){
+    return numero % 2;
 }
