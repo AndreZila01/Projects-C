62696849a1ecd3b7ea7a5facd459459e7a364c1f
BotNintendo <andrecustodiotablet@gmail.com>
2022-11-13 22:47:48 +0000

    Try understand code c
    Tentar entender os char e perceber como enviar os inputs para a variavel char*.
    COM ERRROS

--- Projetos/Projeto 1 - 4 em linha/src/main.c
+++ Projetos/Projeto 1 - 4 em linha/src/main.c
@@ -1,57 +1,64 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-
-struct DadosPessoa
-{
-    char* Nome[50];
-    int Jogos;
-    int Vitorias;
+struct Jogador {
+    char Nome [50];
+    int Vitoria;
+    int Jogadas;
 };
 
+struct Jogador jogador [50];
+
+
+void RegistarJog(){
+    //strcpy(arr[idx].name, name);
+    //strcpy(jogador[sizeof(jogador)/sizeof(jogador[0])].Nome, "Test");
+    //printf(jogador[0].Nome);
+}
 
 int main()
 {
-    /*char value[] = {"dadsa"};
-
-    char str[80] = "This is - www.tutorialspoint.com - website";
-    const char s[2] = "-";
-    char *token;*/
-
-    /* get the first token */
-    //token = strtok(str, s);
-    //printf("%s", token);
     while (true)
     {
-        char input;
-        scanf("%s", &input);
-        char* token = strtok(input, ' ');
+        //char cargo[300];
+        char* cargo;
+        fgets(cargo, 300, stdin);
 
-        if (strcmp(token,"RJ")){
+        if (cargo[0] == 'R' && cargo[1] == 'J')
+            RegistarJog();
+        else if (cargo[0] == 'E' && cargo[1] == 'J')
+            puts("Chefe");
+        else if (cargo[0] == 'L' && cargo[1] == 'J')
+            puts("operario");
+        else if (cargo[0] == 'D')
+            puts("inspetor");
+        else if (cargo[0] == 'I' && cargo[1] == 'J')
+            puts("inspetor");
+        else if (cargo[0] == 'D' && cargo[1] == 'J')
+            puts("mecanico");
+        else if (cargo[0] == 'C' && cargo[1] == 'P')
+            puts("mecanico");
+        else if (cargo[0] == 'V' && cargo[1] == 'R')
+            puts("mecanico");
+        else if (cargo[0] == 'G')
+            puts("mecanico");
+        else if (cargo[0] == 'L')
+            puts("mecanico");
+        else
+            puts("O valor inserido nao tem correspondencia.");
+
+        /*char str[] = "Geeks for Geeks";
+        char* token;
+        char* rest = str;*/
+
+        char *string;
+        char *tofree;
+
+        string = strdup(cargo);
+
+        if (string != NULL)
+            while ((tofree = strsep(&string, " ")) != NULL)
+                printf("%s\n", tofree);
         
-            for(int count = 0; count<sizeof(struct DadosPessoa)/sizeof(struct DadosPessoa[0]); count++){
-                //if(strcmp(struct DadosPessoa[0].Nome, strtok(token, "")));
-            }
-            
-            
-        }
-        else if (strcmp(token, "EJ"))
-            printf("ds");
-        else if (strcmp(token,"LJ"))
-            printf("ds");
-        else if (strcmp(token, "IJ"))
-            printf("ds");
-        else if (strcmp(token,"D"))
-            printf("ds");
-        else if (strcmp(token,"DJ"))
-            printf("ds");
-        else if (strcmp(token,"CP"))
-            printf("ds");
-        else if (strcmp(token,"VR"))
-            printf("ds");
-        else if (strcmp(token,"G"))
-            printf("ds");
-        else if (strcmp(token,"L"))
-            printf("ds");
     }
 }
--- Projetos/Projeto 1 - 4 em linha/src/test.c
+++ Projetos/Projeto 1 - 4 em linha/src/test.c
@@ -1,52 +1,25 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
-char *espacamento(char texto[], const char caract[1])
-{
-    return strtok_r(texto, caract, &texto);
+struct person {
+    char name[80];
+    int age;
+};
+
+void register_person(char* name, int age, struct person arr[], int idx) {
+    strcpy(arr[idx].name, name);
+    arr[idx].age = age;
 }
 
-int main()
-{
-    char cargo[150];
-    fgets(cargo, 150, stdin);
+int main() {
+    struct person users[3];
+    register_person("André", 39, users, 0);
+    register_person("Miguel", 49, users, 1);
+    register_person("Daniel", 6, users, 2);
 
-    if (cargo[0] == 'E' && cargo[1] == 'J')
-        puts("Chefe");
-    else if (cargo[0] == 'L' && cargo[1] == 'J')
-        puts("operario");
-    else if (cargo[0] == 'D')
-        puts("inspetor");
-    else if (cargo[0] == 'I' && cargo[1] == 'J')
-        puts("inspetor");
-    else if (cargo[0] == 'D' && cargo[1] == 'J')
-        puts("mecanico");
-    else if (cargo[0] == 'C' && cargo[1] == 'P')
-        puts("mecanico");
-    else if (cargo[0] == 'V' && cargo[1] == 'R')
-        puts("mecanico");
-    else if (cargo[0] == 'G')
-        puts("mecanico");
-    else if (cargo[0] == 'L')
-        puts("mecanico");
-    else
-        puts("O valor inserido nao tem correspondencia.");
 
-    /*char str[] = "Geeks for Geeks";
-    char* token;
-    char* rest = str;*/
-
-    char *string;
-    char *tofree;
-
-    string = strdup(cargo);
-
-    if (string != NULL)
-    {
-        while ((tofree = strsep(&string, " ")) != NULL)
-        {
-            printf("%s\n", tofree);
-        }
+    for(int i=0; i<3; i++) {
+        printf("%s (%d)\n",users[i].name, users[i].age);
     }
+    return 0;
 }
